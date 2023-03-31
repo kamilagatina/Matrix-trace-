306d5696a2498fd609ea38013b25a4e75566e516
kamilagatina <kamilavatina37199@gmail.com>
2023-04-01 00:06:18 +0400

    second

--- matrix trace.py
+++ matrix trace.py
@@ -1,3 +1,6 @@
+#The input to the program is a natural number n — the number of rows and columns in the matrix, 
+#then the elements of the matrix (integers) line by line, 
+#separated by a space
 n,matr = int(input()),[]
 for i in range(n):
     matr.append(input().split()) 
