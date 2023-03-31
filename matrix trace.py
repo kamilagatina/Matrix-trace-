4ff50807957d0b7f0c96f938dd397f09faf42cbe
kamilagatina <kamilavatina37199@gmail.com>
2023-04-01 00:13:47 +0400

    fourth

--- matrix trace.py
+++ matrix trace.py
@@ -1,10 +1,8 @@
 #The input to the program is a natural number n — the number of rows and columns in the matrix, 
 #then the elements of the matrix (integers) line by line, 
 #separated by a space
-n,matr = int(input()),[]
+n,track = int(input()),0
+matrix = [input().split() for _ in range(n)] 
 for i in range(n):
-    matr.append(input().split()) 
-sled = 0
-for i in range(n):
-    sled+=int(matr[i][i])
-print(sled) 
+    track+=int(matrix[i][i])
+print(track) 
